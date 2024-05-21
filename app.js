50bb9bba3ff83f195813af77d236b18d0a2ca3ac
misraag <jaiofficial50@gmail.com>
2024-05-21 18:06:47 +0530

    index html file transfered to static public folder

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express()
 // setup static and middleware in this file
 app.use(express.static('./public'))
 
-app.get('/', (req, res)=>{
-    res.status(200).sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-})
+// app.get('/', (req, res)=>{
+//     res.status(200).sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// })
 
 app.all("*", (req, res)=> {
     res.status(404).send('<h1>resource not found</h1>')
