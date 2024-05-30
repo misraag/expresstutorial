0b35e0b2c951fd92a38a438b3bbfecee11d9730f
misraag <jaiofficial50@gmail.com>
2024-05-30 18:13:28 +0530

    fetching data that is required through destructuring

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-//Outputing html file through express
 const express = require('express')
-const path = require('path')
-const app = express()
+const app = express();
+const { products } = require('./data.js');
 
-// setup static and middleware in this file
-app.use(express.static('./public'))
-
-// app.get('/', (req, res)=>{
-//     res.status(200).sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-// })
-
-app.all("*", (req, res)=> {
-    res.status(404).send('<h1>resource not found</h1>')
+app.get('/', (req,res)=> {
+    res.send('<h1>Homepage</h1><a href="/api/products">products</a>')
 })
 
+app.get('/api/products', (req, res)=> {
+    const newProducts = products.map((product) => {
+        const {id, name, image } = product;
+        return { id, name, image };
+    })
 
-app.listen(5000, ()=>{
-    console.log("server is listening to port 5000....")
+    res.json(newProducts)
+})
+
+app.listen(5000, ()=> {
+    console.log('server is listening to port 5000....')
 })
