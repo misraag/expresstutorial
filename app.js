65b04c7086a036a7f86db0b824e852e3ad6ee8e8
misraag <jaiofficial50@gmail.com>
2024-05-30 20:17:48 +0530

    query string params to filter out result

--- app.js
+++ app.js
@@ -27,6 +27,30 @@ app.get('/api/products/:productId', (req, res) => {
     return res.json(singleProduct)
 })
 
+app.get('/api/v1/query', (req, res) => {
+    const { search, limit } = req.query
+    let filteredProducts = [...products]
+
+    if(search) {
+        filteredProducts = filteredProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+
+    if(limit) {
+        filteredProducts = filteredProducts.slice(0, Number(limit))
+
+    }
+    if(filteredProducts.length < 1) {
+        return res.status(200).json({'success':true, 'data': []});
+    }
+
+    return res.status(200).json(filteredProducts)
+
+    //Search for http://localhost:5000/api/v1/query?search=a&limit=1 and you will get result that will search for product that has name starting with a and will limit the output to 1 product
+
+})
+
 app.get('/api/products')
 
 app.listen(5000, ()=> {
