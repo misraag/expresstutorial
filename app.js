2706a67bcd3d0f957f81d70f864535500be05ada
misraag <jaiofficial50@gmail.com>
2024-06-04 15:51:21 +0530

    put request

--- app.js
+++ app.js
@@ -37,6 +37,26 @@ app.post("/login", (req, res) => {
   res.status(401).send("Please provide credentials");
 });
 
+app.put('/api/people/:id', (req, res)=>{
+    const { id } = req.params
+    const { name } = req.body
+
+    const person = people.find((person) => person.id === Number(id))
+    if(!person) {
+        res.status(404).json({ success:false, msg: `No person with id ${id}`})
+    }
+
+    const newPeople = people.map((person) => {
+        if(person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success:true, data: newPeople})
+})
+
+
+
 app.listen(5000, () => {
   console.log("Server is listening to port 5000.....");
 });
