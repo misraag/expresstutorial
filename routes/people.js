efc46f9c7dad0489536a78bebb3cf314fec09c81
misraag <jaiofficial50@gmail.com>
2024-06-04 17:05:19 +0530

    added router.route for cleaner code

--- routes/people.js
+++ routes/people.js
@@ -8,10 +8,14 @@ const {
   deletePeople,
 } = require("../controllers/people");
 
-router.get("/", getPeople);
-router.post("/", createPeople);
-router.post("/postman", createPeoplePostman);
-router.put("/:id", updatePeople);
-router.delete("/:id", deletePeople);
+// router.get("/", getPeople);
+// router.post("/", createPeople);
+// router.post("/postman", createPeoplePostman);
+// router.put("/:id", updatePeople);
+// router.delete("/:id", deletePeople);
+
+router.route('/').get(getPeople).post(createPeople)
+router.route('/postman').post(createPeoplePostman)
+router.route('/:id').put(updatePeople).delete(deletePeople)
 
 module.exports = router;
