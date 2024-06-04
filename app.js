04488ec197d603a4d46056c42d09dc35ddd1fbea
misraag <jaiofficial50@gmail.com>
2024-06-04 14:41:28 +0530

    used express.json() to add payload from the http request through javascript to the request body

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ let { people } = require("./data");
 
 app.use(express.static("./methods-public"));
 app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+app.post('/api/people', (req, res) => {
+    const { name } = req.body
+    if(!name) {
+        res.status(400).json({ success:false, msg: 'Please provide name value!'})
+    }
+
+    res.status(201).json({success:true, person:name})
+})
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
